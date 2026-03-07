35594e611656dd73b34cc30d82a5931fcba6e4e5
mwinamijr <mwinamijr@gmail.com>
2026-03-08 00:55:42 +0300

    Add .gitignore, main application file, and update requirements

---
+++ app/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+
+#Initialize app
+app = FastAPI(
+    title="Learn Mathematics Visually.",
+    description="An API for visual mathematics learing.",
+    version="1.0.0",
+)
+
+app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to the Visual mathematics learning API."}
